b6501c10be252cd01392d1f6f9680bf744780eca
KaenCS <60157651+KaenCS@users.noreply.github.com>
2022-11-09 19:35:34 -0500

    pulling from main

--- public/js/student.js
+++ public/js/student.js
@@ -1,6 +1,8 @@
+import { getLoggedInUser } from "./getLoggedInUser.js";
+
 const url = window.location.href;
 const splitUrl = url.split('/');
-const student = splitUrl[splitUrl.length - 2];
+// const student = splitUrl[splitUrl.length - 2];
 
 let studentData = {};
 let studentPrevCourses = {};
@@ -25,42 +27,56 @@ function getStudentData(student) {
 }
 
 function setDataFields(data, prevCourses, currCourses) {
-    nameField.value = data.real_name;
-    aboutField.value = data.description;
-    contactField.value = data.contact;
-    previousCoursesField.value = prevCourses.data;
-    currentCoursesField.value = currCourses.data;
+    nameField.innerHTML = `<br /><br />
+                            <h3>Name:</h3>
+                            <h3>${data.real_name}</h3>`;
+    
+    aboutField.innerHTML = `About Me: <br> ${data.description}`;
+
+    contactField.innerHTML = `Contact Information: <br> ${data.contact_info}`;
+
+    previousCoursesField.innerHTML = prevCourses.data;
+
+    currentCoursesField.innerHTML = currCourses.data;
+
     profilePictureField.src = data.profile_picture;
 }
 
-(async (student) => {
+getLoggedInUser().then(async (student) => {
 
-    console.log("starting student code: " + student)
+    console.log("starting student code: " + student);
+    console.log()
     const url = `/api/users/ + ${student}`;
     const r = await fetch(url, {
         headers: {
             Accept: "application/json",
             "Content-Type": "application/json"
-        }
+        },
     });
+    
+    console.log("starting prevCourses code: " + student);
     const prevUrl = `/api/users/${student}/previousCourses`;
     const prevCourses = await fetch(prevUrl, {
         headers: {
             Accept: "application/json",
             "Content-Type": "application/json"
-        }
+        },
     });
+    
+    console.log("starting currCourses code: " + student);
     const currUrl = `/api/users/${student}/registered_classes`
     const currCourses = await fetch(currUrl, {
         headers: {
             Accept: "application/json",
             "Content-Type": "application/json"
-        }
+        },
     });
+
+    console.log("passed all fetches");
 
     studentData = (await r.json()).data;
     studentPrevCourses = (await prevCourses.json()).data;
     studentCurrCourses = (await currCourses.json()).data;
 
     setDataFields(studentData, studentPrevCourses, studentCurrCourses);
-})
+});
