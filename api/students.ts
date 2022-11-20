4b2e290ed2ad7bd34ebe1de10186b9a72f18169e
KaenCS <60157651+KaenCS@users.noreply.github.com>
2022-11-19 20:17:35 -0500

    update student.ts + edge case handling

--- api/students.ts
+++ api/students.ts
@@ -1,32 +1,46 @@
 import { Request, Response } from "express";
 import { MongoClient } from "mongodb";
 
-export function get_student(req: Request, res: Response) {
+export async function get_student(req: Request, res: Response) {
     console.log(
         `Received API request to get student info for ${req.params.userID}, by user ${req.session.userID}`
     );
     const studentID = parseInt(req.params.userID);
     if (isNaN(studentID) || studentID <= 0) {
-    // Invalid userID in request
-    res.status(400).json({
-        success: false,
-        message: "Invalid student ID",
-    });
+        // Invalid userID in request
+        res.status(400).json({
+            success: false,
+            message: "Invalid student ID",
+        });
+        return;
     }
 
     const client = req.app.locals.client;
 
-    const entry = client
-        .db("users")
-        .collection("profile")
-        .findOne({studentID: studentID});
+    const entry = await helper(client, studentID);    
 
-    const privateProfile = entry.privateProfile;
+    if (entry === null) {
+        res.status(404).json({
+            success: false,
+            message: "Student does not exist",
+        });
+        return;
+    }
+
+    const privateProfile = entry['privateProfile'];
+
     if (!privateProfile || req.session.userID === req.params.userID) {
         res.json({
         success: true,
         data: {
-            
+            username: studentID,
+            real_name: entry['realName'],
+            description: entry['bio'],
+            contact_info: entry['contactInfo'],
+            // looking_for_partners: entry['lookingForPartners'],
+            currentCourses: entry['currentClasses'],
+            previousCourses: entry['previousClasses'],
+            partners: entry['partners'],
         },
         });
     } else {
@@ -35,4 +49,14 @@ export function get_student(req: Request, res: Response) {
         message: "Student's profile is private",
     });
   }
+}
+
+async function helper(client: MongoClient, studentID: any) {
+
+    const res = await client
+    .db("users")
+    .collection("profile")
+    .findOne({studentID: studentID});
+
+    return res;
 }
